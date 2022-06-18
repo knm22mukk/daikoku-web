d72b83585c710089f8fb0e690bc4298dd311f1a8
knm22mukk <knm22mukk@gmail.com>
2022-06-18 23:25:04 +0900

    モバイル用のメニューの設定

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,6 +1,7 @@
 import Image from 'next/image';
 import Link from 'next/link';
 import { FC } from 'react';
+import MobileNav from 'components/MobileNav';
 import { headerNavLinks } from 'data/headerNavLinks';
 
 const Header: FC = () => {
@@ -12,13 +13,16 @@ const Header: FC = () => {
         </a>
       </Link>
       <nav className='flex justify-around'>
-        {headerNavLinks.map((link) => (
-          <Link href={link.title} key={link.href}>
-            <a className='p-3 font-bold hover:text-green-600 border-b-2 border-transparent hover:border-green-600 duration-300'>
-              {link.title}
-            </a>
-          </Link>
-        ))}
+        <div className='hidden md:block'>
+          {headerNavLinks.map((link) => (
+            <Link href={link.href} key={link.href}>
+              <a className='p-3 font-medium hover:text-green-600 border-b-2 border-transparent hover:border-green-600 duration-300'>
+                {link.title}
+              </a>
+            </Link>
+          ))}
+        </div>
+        <MobileNav />
       </nav>
     </header>
   );
---
+++ src/components/MobileNav.tsx
@@ -0,0 +1,45 @@
+import Link from 'next/link';
+import { FC, useState } from 'react';
+import { MdClose, MdMenu } from 'react-icons/md';
+import { headerNavLinks } from 'data/headerNavLinks';
+
+const MobileNav: FC = () => {
+  const [open, setOpen] = useState<boolean>(false);
+
+  const handleClick = () => {
+    setOpen(!open);
+  };
+
+  return (
+    <div className='md:hidden'>
+      <button
+        aria-label='モバイルメニュー'
+        type='button'
+        onClick={handleClick}
+        className='flex items-center text-3xl'
+      >
+        {open ? <MdClose /> : <MdMenu />}
+      </button>
+      <nav
+        className={`fixed w-full h-full top-24 right-0 bg-gray-100 opacity-95 z-10 transform ease-in-out duration-300 ${
+          open ? 'translate-x-0' : 'translate-x-full'
+        }`}
+      >
+        <div className='py-7'>
+          {headerNavLinks.map((link) => (
+            <Link href={link.href} key={link.href}>
+              <a
+                className='flex justify-center py-4 text-2xl font-bold tracking-wide hover:text-green-600'
+                onClick={handleClick}
+              >
+                {link.title}
+              </a>
+            </Link>
+          ))}
+        </div>
+      </nav>
+    </div>
+  );
+};
+
+export default MobileNav;
