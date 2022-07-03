b8cce8d6e28b1362217288440587767f2441204d
knm22mukk <knm22mukk@gmail.com>
2022-07-03 21:34:03 +0900

    更新情報の個別ページの表示のベースを作成

--- src/pages/news/[id].tsx
+++ src/pages/news/[id].tsx
@@ -1,5 +1,9 @@
 import { GetStaticPaths, GetStaticProps, NextPage } from 'next';
+import Image from 'next/image';
+import Link from 'next/link';
+import Date from 'components/Date';
 import Layout from 'components/Layout';
+import SectionHeader from 'components/SectionHeader';
 import { client } from 'libs/client';
 import { Blog } from 'types/blog';
 
@@ -24,7 +28,32 @@ type Props = {
   blog: Blog;
 };
 const BlogId: NextPage<Props> = ({ blog }) => {
-  return <Layout>{blog.title}</Layout>;
+  return (
+    <Layout>
+      <SectionHeader subHeading='NEWS' title={blog.title} />
+      <div className='container'>
+        <Image
+          src={blog.image.url}
+          width={1200}
+          height={630}
+          alt={blog.title}
+          objectFit='cover'
+          className='rounded-lg'
+        />
+        <p className='my-8 text-lg text-right text-gray-600'>
+          <Date dateString={blog.publishedAt} />
+        </p>
+        <div dangerouslySetInnerHTML={{ __html: `${blog.body}` }} className='' />
+        <div className='py-8'>
+          <Link href='/news'>
+            <a className='py-3 px-5 m-2 text-sm font-bold text-white bg-blue-600 hover:bg-blue-800 rounded-lg'>
+              一覧に戻る
+            </a>
+          </Link>
+        </div>
+      </div>
+    </Layout>
+  );
 };
 
 export default BlogId;
