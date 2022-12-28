b2d18910c615d1bfb45df3593e29f3a77cce5492
knm22mukk <knm22mukk@gmail.com>
2022-12-28 22:59:17 +0900

    eslintの設定を一部追記し、全体をフォーマットしました。

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,21 +1,40 @@
 module.exports = {
-    env: {
-      browser: true,
-      es2021: true,
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: ['plugin:react/recommended', 'standard-with-typescript', 'prettier'],
+  ignorePatterns: ['*.config.js'],
+  overrides: [],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: ['react'],
+  rules: {
+    'react/react-in-jsx-scope': 'off',
+    '@typescript-eslint/no-unused-vars': 'error',
+    'import/order': [
+      'error',
+      {
+        pathGroups: [
+          {
+            pattern: '~/**',
+            group: 'external',
+            position: 'after',
+          },
+        ],
+        'newlines-between': 'always',
+        alphabetize: {
+          order: 'asc',
+          caseInsensitive: false,
+        },
+      },
+    ],
+  },
+  settings: {
+    react: {
+      version: detect,
     },
-    extends: ['plugin:react/recommended', 'standard-with-typescript', 'prettier'],
-    overrides: [],
-    parserOptions: {
-      ecmaVersion: 'latest',
-      sourceType: 'module',
-    },
-    plugins: ['react'],
-    rules: {
-      'react/react-in-jsx-scope': 'off',
-    },
-   settings: {
-     react: {
-       version: detect,
-     },
-    },
-  };
+  },
+};
