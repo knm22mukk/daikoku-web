c40cd95e23313a6b954cb8c231b2e008adb2af86
knm22mukk <knm22mukk@gmail.com>
2023-01-19 23:34:58 +0900

    商品データを投入し、表示できるようにしました

--- src/pages/product/[id].tsx
+++ src/pages/product/[id].tsx
@@ -2,7 +2,11 @@ import { GetStaticPaths, GetStaticProps, NextPage } from 'next';
 import Image from 'next/image';
 import React from 'react';
 
+import { Breadcrumb } from '@/components/Breadcrumb';
+import { PageHeader } from '@/components/PageHeader';
 import { PageTemplete } from '@/components/PageTemplete';
+import { SEO } from '@/components/SEO';
+import { siteData } from '@/data/siteData';
 import { client } from '@/libs/client';
 import { Product } from '@/types/Product';
 
@@ -11,15 +15,17 @@ interface Props {
 }
 
 export const getStaticPaths: GetStaticPaths = async () => {
-  const data = await client.getList<Product>({ endpoint: 'products' });
+  const data = await client.getList<Product>({ endpoint: 'products', queries: { limit: 1000 } });
   const paths = data.contents.map((content: { id: string }) => `/product/${content.id}`);
   return { paths, fallback: false };
 };
 
 export const getStaticProps: GetStaticProps = async (context) => {
-  const id = String(context.params?.id);
-  const data = await client.getListDetail<Product>({ endpoint: 'products', contentId: id });
-
+  const id = context.params?.id as string;
+  const data = await client.getListDetail<Product>({
+    endpoint: 'products',
+    contentId: id,
+  });
   return {
     props: {
       product: data,
@@ -30,6 +36,13 @@ export const getStaticProps: GetStaticProps = async (context) => {
 const ProductId: NextPage<Props> = ({ product }) => {
   return (
     <PageTemplete>
+      <SEO
+        title={`${product.name} || 大黒工業株式会社`}
+        description='大黒工業株式会社の商品カテゴリー一覧ページです。'
+        path={`${siteData.url}/product//${product.id}`}
+      />
+      <Breadcrumb lists={[{ title: '製品一覧', path: '/product' }, { title: product.name }]} />
+      <PageHeader subHeading='products' title={product.name} />
       <div className='container py-8'>
         <div>{product.id}</div>
         <div>{product.name}</div>
@@ -38,7 +51,11 @@ const ProductId: NextPage<Props> = ({ product }) => {
         <div>{product.quantity}</div>
         <div>{product.description}</div>
         <div>{product.is_newitem}</div>
-        <Image src={product.image.url} width={500} height={500} alt={product.name} />
+        {product.image != null ? (
+          <Image src={product.image.url} width={400} height={400} alt={product.name} />
+        ) : (
+          <Image src='/images/dammy400x400.png' width={400} height={400} alt={product.name} />
+        )}
       </div>
     </PageTemplete>
   );
