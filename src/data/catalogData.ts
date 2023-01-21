95a6999a0991d850562c7a361fb7cb36972f031f
knm22mukk <knm22mukk@gmail.com>
2023-01-21 17:09:00 +0900

    カタログ一覧に5冊カタログを追加しました

---
+++ src/data/catalogData.ts
@@ -0,0 +1,34 @@
+export const catalogData = [
+  {
+    image: '/images/PickingCATALOG_11.jpg',
+    title: 'ピッキングカタログ',
+    href: 'https://dcs4.icata.net/iportal/CatalogViewInterfaceStartUpAction.do?method=startUp&mode=PAGE&volumeID=DKU00001&catalogId=708760000&pageGroupId=1&designID=DKUD001&catalogCategoryId=&designConfirmFlg=&pagePosition=R',
+    description: '大黒工業の在庫品、ピッキング出荷可能商品はこちらからご確認いただけます。',
+  },
+  {
+    image: '/images/osechi_2022.jpg',
+    title: 'おせちカタログ2022',
+    href: 'https://app.icata.net/web/catalog/view/DKU00001/DKU00001/1632760000',
+    description: '2022年度版のおせちカタログのご案内です。',
+  },
+  {
+    image: '/images/cutlury_0001.jpg',
+    title: 'カトラリーカタログ',
+    href: 'https://app.icata.net/web/catalog/view/DKU00001/DKU00001/1156960000',
+    description:
+      'スプーンやフォークなどのカトラリーのカタログです。プラスチック、木製など幅広く取り扱いをしています。',
+  },
+  {
+    image: '/images/paper_package_0001.jpg',
+    title: '紙パッケージカタログ',
+    href: 'https://app.icata.net/web/catalog/view/DKU00001/DKU00001/1156970000',
+    description:
+      'パルプモールドなどの紙パッケージを掲載しているカタログです。環境配慮型商品を多く掲載しています。',
+  },
+  {
+    image: '/images/paper_package_0001.jpg',
+    title: '伝票カタログ',
+    href: 'https://app.icata.net/web/catalog/view/DKU00001/DKU00001/1087160000',
+    description: '会計表、領収書などの伝票関連を掲載しているカタログです。',
+  },
+];
--- src/pages/catalog/index.tsx
+++ src/pages/catalog/index.tsx
@@ -1,9 +1,12 @@
 import { NextPage } from 'next';
+import Image from 'next/image';
+import { AiOutlineArrowRight } from 'react-icons/ai';
 
 import { Breadcrumb } from '@/components/Breadcrumb';
 import { PageHeader } from '@/components/PageHeader';
 import { PageTemplete } from '@/components/PageTemplete';
 import { SEO } from '@/components/SEO';
+import { catalogData } from '@/data/catalogData';
 import { siteData } from '@/data/siteData';
 
 const index: NextPage = () => {
@@ -16,6 +19,32 @@ const index: NextPage = () => {
       />
       <Breadcrumb lists={[{ title: 'カタログ一覧' }]} />
       <PageHeader subHeading='catalog' title='カタログ一覧' />
+      <div className='container py-8 max-w-7xl'>
+        <div className='flex flex-wrap'>
+          {catalogData.map((catalog, index) => (
+            <div key={index} className='p-4 lg:w-1/2 md:w-full'>
+              <div className='flex border-2 rounded-lg border-gray-300 border-opacity-50 p-8 sm:flex-row flex-col'>
+                <div className='sm:mr-8 sm:mb-0 mb-4 inline-flex items-center justify-center flex-shrink-0'>
+                  <Image src={catalog.image} height={150} width={106} alt={catalog.title} />
+                </div>
+                <div className='flex-grow'>
+                  <h2 className='text-lg font-bold mb-3'>{catalog.title}</h2>
+                  <p className='leading-relaxed text-base'>{catalog.description}</p>
+                  <a
+                    href={catalog.href}
+                    target='_blank'
+                    className='mt-3 text-indigo-500 inline-flex items-center'
+                    rel='noreferrer'
+                  >
+                    カタログを見る
+                    <AiOutlineArrowRight className='ml-2' />
+                  </a>
+                </div>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
     </PageTemplete>
   );
 };
