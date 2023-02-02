c409247418cf9a681568561b9116f9c0cd89816c
knm22mukk <knm22mukk@gmail.com>
2023-02-02 22:29:36 +0900

    CTAセクションの表示崩れを修正しました

--- src/components/Cta.tsx
+++ src/components/Cta.tsx
@@ -5,7 +5,7 @@ export const Cta: FC = () => {
   return (
     <section className='my-16'>
       <div className='py-16 bg-gray-300 max-w-7xl container rounded-lg'>
-        <div className='px-4 mx-auto text-center sm:px-6 lg:px-8'>
+        <div className='text-center'>
           <h2 className='text-3xl font-bold tracking-tight sm:text-4xl md:text-5xl'>
             大黒工業はSNSでも発信します
           </h2>
@@ -14,12 +14,12 @@ export const Cta: FC = () => {
             <br />
             是非、下記リンクからフォローをお願いいたします。
           </p>
-          <div className='flex justify-center mt-8 space-x-3'>
+          <div className='flex justify-center items-center flex-col lg:flex-row mt-8'>
             <a
               href='https://www.instagram.com/daikoku1950_official/'
               target='_blank'
               rel='noreferrer'
-              className='text-white bg-[#f1224d] hover:bg-[#f1224d]/90 font-medium rounded-lg text-lg px-5 py-2.5 text-center inline-flex items-center mr-2 mb-2'
+              className='max-w-[180px] justify-center text-white bg-[#f1224d] hover:bg-[#f1224d]/90 font-medium rounded-lg text-lg px-5 py-2.5 text-center inline-flex items-center mr-2 mb-2'
             >
               <AiOutlineInstagram className='mr-2 w-7 h-7' />
               Instagram
@@ -28,7 +28,7 @@ export const Cta: FC = () => {
               href='https://twitter.com/daikoku_office'
               target='_blank'
               rel='noreferrer'
-              className='text-white bg-[#1da1f2] hover:bg-[#1da1f2]/90 font-medium rounded-lg text-lg px-5 py-2.5 text-center inline-flex items-center mr-2 mb-2'
+              className='max-w-[180px] justify-center text-white bg-[#1da1f2] hover:bg-[#1da1f2]/90 font-medium rounded-lg text-lg px-5 py-2.5 text-center inline-flex items-center mr-2 mb-2'
             >
               <AiOutlineTwitter className='w-7 h-7 mr-2' />
               Twitter
@@ -37,9 +37,9 @@ export const Cta: FC = () => {
               href='https://www.youtube.com/@daikoku_industry'
               target='_blank'
               rel='noreferrer'
-              className='text-white bg-[#FF0000] hover:bg-[#FF0000]/90 font-medium rounded-lg text-lg px-5 py-2.5 text-center inline-flex items-center mr-2 mb-2'
+              className='max-w-[180px] justify-center text-white bg-[#FF0000] hover:bg-[#FF0000]/90 font-medium rounded-lg text-lg px-5 py-2.5 text-center inline-flex items-center mr-2 mb-2'
             >
-              <AiOutlineYoutube className='mr-2 inline-flex w-7 h-7 items-center' />
+              <AiOutlineYoutube className='mr-2 w-7 h-7' />
               Youtube
             </a>
           </div>
